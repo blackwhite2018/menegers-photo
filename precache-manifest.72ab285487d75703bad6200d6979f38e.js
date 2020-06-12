7a9d00b55c7818f17f32cd979bfb947cb3e6fc53
Sergei M <black_white2018@rambler.ru>
2020-06-13 00:31:12 +0300

    Updates

---
+++ precache-manifest.72ab285487d75703bad6200d6979f38e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "971431ab850c0594b9f2716e82e5ea37",
+    "url": "/menegers-photo/index.html"
+  },
+  {
+    "revision": "5c8c50f4329785967fb6",
+    "url": "/menegers-photo/static/css/main.392a17f8.chunk.css"
+  },
+  {
+    "revision": "69d5ed330db1d7fe9b08",
+    "url": "/menegers-photo/static/js/2.6209c392.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/menegers-photo/static/js/2.6209c392.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c8c50f4329785967fb6",
+    "url": "/menegers-photo/static/js/main.cf81905f.chunk.js"
+  },
+  {
+    "revision": "b9c5f158dab844fdca2a",
+    "url": "/menegers-photo/static/js/runtime-main.f74028f0.js"
+  }
+]);
